664e28c39efdc6b1fb9aa6a1bec13076d821083d
Invisiblesmell <evanwhytock@icloud.com>
2026-07-13 20:46:03 +0100

    Connect Loopbox to Firebase

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
-// Paste the public Firebase web-app configuration for Loopbox here.
-// These values identify the Firebase project; they are safe to use in browser code.
+// Public Firebase web-app configuration for Loopbox.
 window.LOOPBOX_FIREBASE_CONFIG = {
-  apiKey: "",
-  authDomain: "",
+  apiKey: "AIzaSyALDR28kPS2q4oqsDLvLLqZ3u3dx71wgdY",
+  authDomain: "loopbox-3507c.firebaseapp.com",
   databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  projectId: "loopbox-3507c",
+  storageBucket: "loopbox-3507c.firebasestorage.app",
+  messagingSenderId: "282638658375",
+  appId: "1:282638658375:web:67826e27c03e1e28f5ba62",
+  measurementId: "G-P9DPYWZRP5",
 };
